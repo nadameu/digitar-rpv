c28ecdb961d3c5843c6621c1b4451598f24a3048
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-03-21 19:07:02 -0300

    Novos testes

---
+++ src/obterElementosFactory.test.ts
@@ -0,0 +1,41 @@
+import obterElementosFactory from './obterElementosFactory';
+
+const makeFakeDoc = elementos => ({
+  getElementById(id: string) {
+    return elementos[id];
+  },
+});
+
+describe('obterElementosFactory', () => {
+  it('Funciona se todos os elementos existirem', done => {
+    const elementos: { [id: string]: any } = { a: {}, b: {}, c: {} };
+    const ids = Object.keys(elementos);
+    const elementosAsArray = ids.map(id => elementos[id]);
+
+    const fakeDoc = makeFakeDoc(elementos);
+    const obterElementos = obterElementosFactory(fakeDoc as any);
+
+    return obterElementos(ids).then(els => {
+      expect(els).toEqual(elementosAsArray);
+      done();
+    });
+  });
+
+  it('Falha se algum elemento não existir', done => {
+    const elementos: { [id: string]: any } = { a: {}, b: null, c: null, d: {} };
+    const ids = Object.keys(elementos);
+    const primeiroIdNulo = ids.reduce(
+      (acc: string | null, id) =>
+        acc === null && elementos[id] === null ? id : acc,
+      null
+    );
+
+    const fakeDoc = makeFakeDoc(elementos);
+    const obterElementos = obterElementosFactory(fakeDoc as any);
+
+    return obterElementos(ids).catch(e => {
+      expect(e).toEqual(primeiroIdNulo);
+      done();
+    });
+  });
+});
