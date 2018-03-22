c41e49d64289db96d4652f8eb3c66c86eeccc8f1
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-03-22 13:18:56 -0300

    Dependências no arquivo index

--- src/appFactory.ts
+++ src/appFactory.ts
@@ -1,10 +1,6 @@
 //@ts-ignore
 import formulario from './formulario.html';
-import limparTabIndexFactory from './limparTabIndexFactory';
-import obterElementosFactory from './obterElementosFactory';
 import vincularSoma from './vincularSoma';
-import tentarAteEncontrarFactory from './tentarAteEncontrarFactory';
-import tentarAteEncontrarComValorFactory from './tentarAteEncontrarComValorFactory';
 import preencherFormulario from './preencherFormulario';
 
 const divs = ['divCalcValsBrutos'];
@@ -22,51 +18,47 @@ const inputs = [
 ];
 const buttons = ['btnAplicarCalcValsBrutos'];
 
-const appFactory = (doc: Document) => {
-	const limparTabIndex = limparTabIndexFactory(doc);
-	const obterElementos = obterElementosFactory(doc);
-	const tentarAteEncontrar = tentarAteEncontrarFactory(obterElementos);
-	const tentarAteEncontrarComValor = tentarAteEncontrarComValorFactory(
-		obterElementos
-	);
-
-	return () => {
-		return tentarAteEncontrar(divs).then(([div]) => {
-			div.insertAdjacentHTML('beforeend', formulario);
-			return Promise.all([
-				tentarAteEncontrar(buttons) as Promise<HTMLButtonElement[]>,
-				tentarAteEncontrarComValor(inputs) as Promise<HTMLInputElement[]>,
-			]).then(([buttons, inputs]) => {
-				const [aplicar] = buttons;
-				const [
-					inputBrutoTotal,
-					inputBrutoPrincipal,
-					inputBrutoJuros,
-					inputBrutoCorrente,
-					inputBrutoAnterior,
-					inputTotal,
-					inputPrincipal,
-					inputJuros,
-					inputCorrente,
-					inputAnterior,
-				] = inputs;
-				limparTabIndex();
-				vincularSoma(inputBrutoPrincipal, inputBrutoJuros, inputBrutoTotal);
-				vincularSoma(inputPrincipal, inputJuros, inputTotal);
+const appFactory = (
+	limparTabIndex: () => void,
+	tentarAteEncontrar: (ids: string[]) => Promise<HTMLElement[]>,
+	tentarAteEncontrarComValor: (ids: string[]) => Promise<HasValue[]>
+) => () => {
+	return tentarAteEncontrar(divs).then(([div]) => {
+		div.insertAdjacentHTML('beforeend', formulario);
+		return Promise.all([
+			tentarAteEncontrar(buttons) as Promise<HTMLButtonElement[]>,
+			tentarAteEncontrarComValor(inputs) as Promise<HTMLInputElement[]>,
+		]).then(([buttons, inputs]) => {
+			const [aplicar] = buttons;
+			const [
+				inputBrutoTotal,
+				inputBrutoPrincipal,
+				inputBrutoJuros,
+				inputBrutoCorrente,
+				inputBrutoAnterior,
+				inputTotal,
+				inputPrincipal,
+				inputJuros,
+				inputCorrente,
+				inputAnterior,
+			] = inputs;
+			limparTabIndex();
+			vincularSoma(inputBrutoPrincipal, inputBrutoJuros, inputBrutoTotal);
+			vincularSoma(inputPrincipal, inputJuros, inputTotal);
+			preencherFormulario(
+				inputBrutoTotal,
+				[inputCorrente, inputAnterior],
+				[inputBrutoCorrente, inputBrutoAnterior]
+			);
+			aplicar.addEventListener('click', () => {
 				preencherFormulario(
-					inputBrutoTotal,
-					[inputCorrente, inputAnterior],
-					[inputBrutoCorrente, inputBrutoAnterior]
+					inputTotal,
+					[inputBrutoCorrente, inputBrutoAnterior],
+					[inputCorrente, inputAnterior]
 				);
-				aplicar.addEventListener('click', () => {
-					preencherFormulario(
-						inputTotal,
-						[inputBrutoCorrente, inputBrutoAnterior],
-						[inputCorrente, inputAnterior]
-					);
-				});
 			});
 		});
-	};
+	});
 };
+
 export default appFactory;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import './estilos.scss';
 import appFactory from './appFactory';
+import limparTabIndexFactory from './limparTabIndexFactory';
+import obterElementosFactory from './obterElementosFactory';
+import tentarAteEncontrarFactory from './tentarAteEncontrarFactory';
+import tentarAteEncontrarComValorFactory from './tentarAteEncontrarComValorFactory';
 
-const app = appFactory(document);
+const limparTabIndex = limparTabIndexFactory(document);
+const obterElementos = obterElementosFactory(document);
+const tentarAteEncontrar = tentarAteEncontrarFactory(obterElementos);
+const tentarAteEncontrarComValor = tentarAteEncontrarComValorFactory(
+	obterElementos
+);
+
+const app = appFactory(
+	limparTabIndex,
+	tentarAteEncontrar,
+	tentarAteEncontrarComValor
+);
 app().catch(e => console.error(e));
