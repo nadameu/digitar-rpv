1a5069a89f2e8604c422780855d0cf75651a597c
Paulo R. Maurici Jr <paulo@nadameu.com.br>
2018-04-19 19:42:00 -0300

    Conversão de metadados

--- metadata.js
+++ metadata.js
@@ -2,7 +2,7 @@ export default {
 	description: 'Auxilia a conferência de RPVs e precatórios.',
 	namespace: 'http://nadameu.com.br/',
 	include: [
-		'/^https:\\/\\/eproc\\.(jf(pr|rs|sc)|trf4)\\.jus\\.br\\/eproc(V2|2trf4)\\/controlador\\.php\\?acao=oficio_requisitorio_beneficiarioshonorarios_editar&/',
+		/^https:\/\/eproc\.(jf(pr|rs|sc)|trf4)\.jus\.br\/eproc(V2|2trf4)\/controlador\.php\?acao=oficio_requisitorio_beneficiarioshonorarios_editar&/,
 	],
 	grant: 'none',
 	'run-at': 'document-start',
